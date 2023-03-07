053e0739f6336114afc3217885a8e2b9f861ef60
Redapo <69197418+Redapo15@users.noreply.github.com>
2023-03-07 19:56:54 +0200

    calculator
    works but not with al the inputs

--- calculator.py
+++ calculator.py
@@ -5,9 +5,8 @@ summ = 0
 while True:
     str1 = str(input("give calculation to solve:  "))
     li = list(str1.split(" "))
-    #print(li)
-    for i in range(len(li)):
-        print(i)
+    leng = len(li)
+    for i in range(leng):
         if i%2!=0:
             if li[i] == "*":
                 res = float(li[i-1])*float(li[i+1])
@@ -19,14 +18,14 @@ while True:
                 del(li[i-1])
                 del(li[i+1-1])
                 li[i-1]=res  
-    else:
-        summ = float(li[0])
-        for a in range(2,len(li)):
-            if a%2==0:
-                if li[a-1]=="+":
-                    summ += float(li[a])
-                elif li[a-1]=="-":
-                    summ -= float(li[a])
+            leng = len(li)
+    summ = float(li[0])
+    for a in range(2,len(li)):
+        if a%2==0:
+            if li[a-1]=="+":
+                summ += float(li[a])
+            elif li[a-1]=="-":
+                summ -= float(li[a])
     print(f"answer: {summ}")
     level2=0
     lvl2=[]
